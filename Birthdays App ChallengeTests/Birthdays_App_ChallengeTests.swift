095bd5497f91f8ac9815efaa32d1a4e0b2c8fcba
Pete Chambers <pcbchambers@gmail.com>
2022-02-04 09:33:40 +0000

    include unit test

--- Birthdays App ChallengeTests/Birthdays_App_ChallengeTests.swift
+++ Birthdays App ChallengeTests/Birthdays_App_ChallengeTests.swift
@@ -10,20 +10,29 @@ import XCTest
 
 class Birthdays_App_ChallengeTests: XCTestCase {
 
+    private var sut: WebService!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = WebService()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAPIResponse() throws {
+        let expectation = XCTestExpectation(description: "Alamofire")
+        sut?.fetchBirthdays { (result) in
+            switch result {
+            case .success(let birthdays):
+                XCTAssertEqual(birthdays.results.count, 1000)
+                expectation.fulfill()
+            case .failure(let error):
+                debugPrint("test failed with error: \(error)")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+
     }
 
     func testPerformanceExample() throws {
